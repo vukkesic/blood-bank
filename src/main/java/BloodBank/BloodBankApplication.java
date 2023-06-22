2a8e11b83bf10393d290f43768f7c94dde0f61b2
Vuk Kesic <vukk.98@gmail.com>
2023-06-22 13:51:22 +0200

    Date changed

--- src/main/java/BloodBank/BloodBankApplication.java
+++ src/main/java/BloodBank/BloodBankApplication.java
@@ -17,8 +17,8 @@ public class BloodBankApplication {
         String s3 = "27-Aug-2023 7:00:00";
         String s4 = "27-Aug-2023 15:00:00";
         SimpleDateFormat formatter=new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
-        BloodDonationNotificationDTO bdn = new BloodDonationNotificationDTO("Obavestenje", "Obavestavamo vas da ce dana 15.05.2023. od 9-13h organizovati akcija ddk u mestu Lok.", formatter.parse(s1), formatter.parse(s2), "Lok");
-        BloodDonationNotificationDTO bdn2 = new BloodDonationNotificationDTO("Obavestenje", "Obavestavamo vas da ce dana 27.08.2023. od 7-15h organizovati akcija ddk u mestu Kacarevo.", formatter.parse(s3), formatter.parse(s4), "Kacarevo");
+        BloodDonationNotificationDTO bdn = new BloodDonationNotificationDTO("Obavestenje", "Obavestavamo vas da ce dana 07.007.2023. od 9-13h organizovati akcija ddk u mestu Lok.", formatter.parse(s1), formatter.parse(s2), "Lok");
+        BloodDonationNotificationDTO bdn2 = new BloodDonationNotificationDTO("Obavestenje", "Obavestavamo vas da ce dana 27.08.2023. od 7-15h organizovati akcija ddk u mestu Futog.", formatter.parse(s3), formatter.parse(s4), "Futog");
         try {
             BloodBank.SendingNotifications.sendNotification(bdn);
             BloodBank.SendingNotifications.sendNotification(bdn2);
